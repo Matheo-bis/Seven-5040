2185ab6ce7fc7716d304244ac9a7e3de6125996d
justi <justin.sottile@uha.fr>
2022-06-08 17:11:14 +0200

    commit commit

--- app/src/main/java/fr/seven/mathgame/Debutant.java
+++ app/src/main/java/fr/seven/mathgame/Debutant.java
@@ -16,8 +16,8 @@ public class Debutant extends Equation{
              String StringSumx=String.valueOf(Sumx);
              String StringSumy=String.valueOf(Sumy);
              int SumResultat = Sumx+Sumy;
-             String SumEquation=StringSumx +operateur + StringSumy + "=__";
-             return SumEquation;
+             resultat(SumResultat);
+             return StringSumx +operateur + StringSumy +"="+new String(new char[String.valueOf(SumResultat).length()]).replace('\u0000', '_');
 
          case 1:
              operateur = "-";
@@ -30,9 +30,9 @@ public class Debutant extends Equation{
              }
              String StringMinx=String.valueOf(Minx);
              String StringMiny=String.valueOf(Miny);
-             int MinResultat = Minx+Miny;
-             String MinEquation=StringMinx +operateur + StringMiny + "=__";
-             return MinEquation;
+             int MinResultat = Minx-Miny;
+             resultat(MinResultat);
+             return StringMinx +operateur + StringMiny+"="+ new String(new char[String.valueOf(MinResultat).length()]).replace('\u0000', '_');
 
          case 2:
              operateur = "x";
@@ -40,13 +40,16 @@ public class Debutant extends Equation{
              int Multy = rand.nextInt(6);
              String StringMultx=String.valueOf(Multx);
              String StringMulty=String.valueOf(Multy);
-             int MultResultat = Multx+Multy;
-             String MultEquation=StringMultx +operateur + StringMulty + "=__";
-             return MultEquation;
+             int MultResultat = Multx*Multy;
+             resultat(MultResultat);
+             return StringMultx +operateur + StringMulty + "="+ new String(new char[String.valueOf(MultResultat).length()]).replace('\u0000', '_');
+
          default:
              operateur = "+";
              return "x+y=_";
      }
  }
-
+static String resultat(int Resultat){
+        return String.valueOf(Resultat);
+}
 }
