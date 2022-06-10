db4b91fe3e84390e22a156224b4e819d596e17d5
NicolasCoudrillier <59061866+NicolasCoudrillier@users.noreply.github.com>
2022-06-10 16:22:49 +0200

    Graphisme amélioré et début du bouton pour le jeu 2 dans le menu

--- app/src/main/java/fr/seven/mathgame/LancementJeu2Activity.java
+++ app/src/main/java/fr/seven/mathgame/LancementJeu2Activity.java
@@ -3,12 +3,20 @@ package fr.seven.mathgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LancementJeu2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lancement_jeu2);
+        Button buttonJouer =(Button) findViewById(R.id.buttonJouer);
+        buttonJouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_lancement_jeu2);
+            }
+        });
     }
 }
