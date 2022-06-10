2ee1085bfbc3f4dbfc32341c8b38715ab9e538f9
Meliodas-1O <fode.cissokho@uha.fr>
2022-06-10 09:55:23 +0200

    calculatrice

--- app/src/main/java/fr/seven/mathgame/CalculatriceActivity.java
+++ app/src/main/java/fr/seven/mathgame/CalculatriceActivity.java
@@ -1,68 +1,299 @@
 package fr.seven.mathgame;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class CalculatriceActivity extends AppCompatActivity {
-
-    private Button[] buttons = new Button[20];
     private TextView resultat;
+    private TextView deuxio;
     private TextView calcul;
-    public char [] sign = {'+','-','x','/'};
-
+    private double num1;
+    private double num2=0;
+    boolean ok=false;
+    private boolean num1_exist = false;
+    private boolean num2_exist = false;
+    private float result;
+    private String operation;
+    private String [] signs = {"+","-","×","÷","!","^"};
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculatrice);
-        setCalcul();
+
+        resultat = findViewById(R.id.resultat);
+        deuxio = findViewById(R.id.terme);
+        calcul = findViewById(R.id.calcul);
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        //TEMPORAIRE
+        //setCalcul();
+    }
+    public double factorial(double n){
+        double result = 1.0;
+        for (double factor = 2.0; factor <= n; factor++) {
+            result = result * factor;
+        }
+        return result;
+
     }
 
-    public void setCalcul(){
-        buttons[0]=(Button)findViewById(R.id.button0);
-        buttons[1]=(Button)findViewById(R.id.button1);
-        buttons[2]=(Button)findViewById(R.id.button2);
-        buttons[3]=(Button)findViewById(R.id.button3);
-        buttons[4]=(Button)findViewById(R.id.button4);
-        buttons[5]=(Button)findViewById(R.id.button5);
-        buttons[6]=(Button)findViewById(R.id.button6);
-        buttons[7]=(Button)findViewById(R.id.button7);
-        buttons[8]=(Button)findViewById(R.id.button8);
-        buttons[9]=(Button)findViewById(R.id.button9);
-        calcul = (TextView) findViewById(R.id.calcul);
-        resultat = (TextView) findViewById(R.id.result);
+    public void buttonclick(View view){
+
+        String viewName = view.getResources().getResourceName(view.getId());
+        String button = "_";
+        String op = operation;
+        switch(viewName) {
+            case "fr.seven.mathapp:id/button1":
+                button = "1";
+                if (num1_exist && !num2_exist){
+                   // deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button2":
+                button = "2";
+                if (num1_exist && !num2_exist){
+                   // deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button3":
+                button = "3";
+                if (num1_exist && !num2_exist){
+                   // deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button4":
+                button = "4";
+                if (num1_exist && !num2_exist){
+                    //deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button5":
+                button = "5";
+                if (num1_exist && !num2_exist){
+                    //deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button6":
+                button = "6";
+                if (num1_exist && !num2_exist){
+                   // deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button7":
+                button = "7";
+                if (num1_exist && !num2_exist){
+                   // deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button8":
+                button = "8";
+                if (num1_exist && !num2_exist){
+                  //  deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button9":
+                button = "9";
+                if (num1_exist && !num2_exist){
+                   // deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/button0":
+                button = "0";
+                if (num1_exist && !num2_exist){
+                    //deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+
+            case "fr.seven.mathapp:id/buttonadd":
+                button = "+";
+                operation = button;
+                if (!num1_exist){
+                    num1_exist=true;
+                    num1 = Double.parseDouble((String)calcul.getText().toString());
+             }
+                else {
+                    if(!num2_exist && op=="!"){
+                        deuxio.setText("0");
+                    }
+                    if(ok){
+                        deuxio.setText("0");
+                    }
+                    num2 = Double.parseDouble((String)deuxio.getText().toString());
+                }
+                break;
+            case "fr.seven.mathapp:id/buttonsub":
+                button = "-";
+                operation =button;
+                if (!num1_exist){
+                    num1_exist=true;
+                    num1 =Double.parseDouble((String)calcul.getText());
+                }
+                else {
+                    if(!num2_exist && op=="!"){
+                        deuxio.setText("0");
+                    }
+                    if(ok){
+                        //deuxio.setText("0");
+                        num2_exist=false;
+                    }
+                    num2 = Double.parseDouble((String)deuxio.getText());
+                }
+                break;
+            case "fr.seven.mathapp:id/buttondiv":
+                button = "÷";
+                operation =button;
+                if (!num1_exist){
+                    num1_exist=true;
+                    num1 =Double.parseDouble((String)calcul.getText());
+                }
+                else {
+                    if(!num2_exist && op=="!"){
+                        deuxio.setText("1");
+                    }
+                    num2 = Double.parseDouble((String)deuxio.getText());
+                }
+                break;
+            case "fr.seven.mathapp:id/buttonmult":
+                button = "×";
+                operation =button;
+                if (!num1_exist){
+                    num1_exist=true;
+                    num1 = Double.parseDouble((String)calcul.getText());
+                }
+                else {
+                    if(!num2_exist && op=="!"){
+                        deuxio.setText("1");
+                    }
+                    num2 = Double.parseDouble((String)deuxio.getText());
+                }
+                break;
+            case "fr.seven.mathapp:id/buttondot":
+                button = ".";
+                if (num1_exist && !num2_exist){
+                    // deuxio.setText(deuxio.getText().toString() + button);
+                }
+                break;
+            case "fr.seven.mathapp:id/buttonfac":
+                button = "!";
+                operation = button;
+                if (!num1_exist){
+                    num1_exist=true;
+                    num1 =Double.parseDouble((String)calcul.getText());
+                    result=(float)factorial((double)num1);
+                    resultat.setText(String.valueOf(result));
+                    num1=result;
+                }
+                else{
+                    num2 = Double.parseDouble((String)deuxio.getText());
+                    num2= (float)(factorial((double)num2));}
+                break;
+            case "fr.seven.mathapp:id/buttonpow":
+                button = "^";
+                operation =button;
+                if (!num1_exist){
+                    num1_exist=true;
+                    num1 = Double.parseDouble((String)calcul.getText());
+                }
+                else {
+                    num2 = Double.parseDouble((String)deuxio.getText());
+                }
+                break;
+        }
+        calcul.setText(calcul.getText().toString() + button);
+        if (num1_exist) {
+            if (button != signs[0] && button != signs[1] && button != signs[2] && button != signs[3]&& button != signs[4] &&button != signs[5]) {
+                deuxio.setText(deuxio.getText().toString() + button);
+                num2 = Double.parseDouble(deuxio.getText().toString());
+            }
+            else if ( (button != signs[0] || button != signs[1] || button != signs[2] || button != signs[3] || button != signs[4] || button != signs[5] ) && (deuxio.getText().toString()!="" &&deuxio.getText().toString()!="0" )){
+                num2_exist=true;
+            }
+
+        }
+        if(num1_exist && num2_exist){
+            switch (op){
+                case "+":
+                    result= (float) (num1+num2);
+                    break;
+                case "-":
+                    result= (float) (num1-num2);
+                    break;
+                case "×":
+                    result=(float) (num1*num2);
+                    break;
+                case "÷":
+                    result= (float) (num1/num2);
+                    break;
+                case "^":
+                    result = (float)Math.pow((double)num1, (double) num2);
+                    break;
+            }
+            num1= result;
+            resultat.setText(String.valueOf(result));
+            num2_exist=false;
+            deuxio.setText("");
+        }
     }
-    public void OnClickZero(View v) {
-        calcul.setText(calcul.getText().toString() + '0');
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void buttonOK(View view){
+        num2_exist=!num2_exist;
+        if(num1_exist && num2_exist) {
+            switch (operation) {
+                case "+":
+                    result = (float) (num1 + num2);
+                    break;
+                case "-":
+                    result = (float) (num1 - num2);
+                    break;
+                case "×":
+                    result = (float) (num1 * num2);
+                    break;
+                case "÷":
+                    result = (float) (num1 / num2);
+                    break;
+                case "!":
+                    result = (float) factorial((int) num1);
+                    break;
+                case "^":
+                    result = (float) Math.pow((double) num1, (double) num2);
+                    break;
+            }
+            num1=result;
+            num2_exist = false;
+            ok=true;
+            resultat.setText(String.valueOf(result));
+            deuxio.setText("");
+        }
+
     }
-    public void OnClickOne(View v) {
-        calcul.setText(calcul.getText().toString() + '1');
+    public void buttonDEL(View view) {
+        if (calcul.length() > 0) {
+            calcul.setText(calcul.getText().toString().substring(0, calcul.length() - 1));
+            if (deuxio.length() > 0) {
+                deuxio.setText(deuxio.getText().toString().substring(0, deuxio.length() - 1));
+            }
+        }
     }
-    public void OnClickTwo(View v) {
-        calcul.setText(calcul.getText().toString() + '2');
+
+    public void buttonCE(View view){
+        calcul.setText("");
+        resultat.setText("");
+        num1 = 0;
+        num1_exist=false;
+        num2_exist=false;
+        num2 = 0;
+        deuxio.setText("");
     }
-    public void OnClickThree(View v) {
-        calcul.setText(calcul.getText().toString() + '3');
-    }
-    public void OnClickFour(View v) {
-        calcul.setText(calcul.getText().toString() + '4');
-    }
-    public void OnClickFive(View v) {
-        calcul.setText(calcul.getText().toString() + '5');
-    }
-    public void OnClickSix(View v) {
-        calcul.setText(calcul.getText().toString() + '6');
-    }
-    public void OnClickSeven(View v) {
-        calcul.setText(calcul.getText().toString() + '7');
-    }
-    public void OnClickEight(View v) {
-        calcul.setText(calcul.getText().toString() + '8');
-    }
-    public void OnClickNine(View v) {
-        calcul.setText(calcul.getText().toString() + '9');
-    }
+
+
 }
