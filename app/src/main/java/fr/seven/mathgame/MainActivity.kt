954f6aa0804a46a74a36f668246239c40ecaa028
BISEL Jean <jean@langmatt.com>
2022-06-14 10:35:47 +0200

    bufgix, le RankingActivity avait disparu du manifeste

--- app/src/main/java/fr/seven/mathgame/MainActivity.kt
+++ app/src/main/java/fr/seven/mathgame/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
+import java.lang.annotation.Native
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -121,4 +122,5 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, CoursActivity::class.java)
         startActivity(intent)
     }
+
 }
