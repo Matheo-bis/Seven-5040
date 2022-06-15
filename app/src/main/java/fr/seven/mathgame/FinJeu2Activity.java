40094f67fa1f08f13c4981538e0f934a04070830
NicolasCoudrillier <59061866+NicolasCoudrillier@users.noreply.github.com>
2022-06-15 14:37:14 +0200

    Plus de crash au retour au menu

--- app/src/main/java/fr/seven/mathgame/FinJeu2Activity.java
+++ app/src/main/java/fr/seven/mathgame/FinJeu2Activity.java
@@ -31,7 +31,7 @@ public class FinJeu2Activity extends AppCompatActivity {
         finish();
     }
 
-    public void goto_jeu2(View view){
+    public void goto_menu(View view){
         finish();
     }
     public void win(View view){
