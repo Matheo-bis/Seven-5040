b6c81fdaf36e5a2c5621038da7ad023056f35066
BISEL Jean <jean@langmatt.com>
2022-06-15 15:31:42 +0200

    changements

--- app/src/main/java/fr/seven/mathgame/LancementJeu2Activity.java
+++ app/src/main/java/fr/seven/mathgame/LancementJeu2Activity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class LancementJeu2Activity extends AppCompatActivity {
 
