b5caca8363bae71a06343962d10e81f43d8cb635
Matheo-bis <matheomarcout@tutamail.com>
2022-06-16 09:54:43 +0200

    Chasse au bugs et purge du code

--- app/src/main/java/fr/seven/mathgame/Jeu3Timer.java
+++ app/src/main/java/fr/seven/mathgame/Jeu3Timer.java
@@ -3,25 +3,13 @@ package fr.seven.mathgame;
 import static java.lang.Math.max;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.Html;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
-
-import java.util.ArrayList;
-import java.util.Random;
 
 public class Jeu3Timer extends Jeu{
     protected int scorejeu3 = 0;
@@ -36,7 +24,6 @@ public class Jeu3Timer extends Jeu{
 
         timebar.setMax(1000);
         timebar.setProgress(1000);
-        int[] currentProgress = {1000};
 
         new Handler().postDelayed(new Runnable() {
         @Override
@@ -51,7 +38,7 @@ public class Jeu3Timer extends Jeu{
                     ((ViewGroup) timebar.getParent()).removeView(timebar);
                     //new Handler().postDelayed(this, 10);
                     Intent intent = new Intent(getApplicationContext(), FinJeu3Activity.class);
-                    intent.putExtra("action", "lose");
+                    intent.putExtra("action", "win");
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     intent.putExtra("numero", numeroJeu);
                     intent.putExtra("score", scorejeu3);
@@ -62,7 +49,7 @@ public class Jeu3Timer extends Jeu{
                 }
             }
             catch(Exception e){
-                System.out.println(e);
+                e.printStackTrace();
             }
         }
     }, 0);
